690b6a02561ebf2d197ebb7e381e03d489bab19a
Alt_nant <131745606+altverse1@users.noreply.github.com>
2023-06-27 15:19:51 +0530

    Add files via upload

---
+++ programs/scrapy/program1.py
@@ -0,0 +1,19 @@
+import scrapy
+
+class WikipediaSpider(scrapy.Spider):
+    name = 'program1'
+    start_urls = ['https://nmamit.nitte.edu.in/']
+    custom_settings = {
+        'USER_AGENT':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
+    }
+
+    def parse(self, response):
+        images = response.css('img::attr(src)').getall()
+        paragraphs = response.css('p::text').getall()
+        with open('images.txt','w') as file:
+            for image in images:
+                file.write(image+'\n')
+        with open('paragraphs.txt','w') as file:
+            for paragraph in paragraphs:
+                file.write(paragraph+'\n')
+            
